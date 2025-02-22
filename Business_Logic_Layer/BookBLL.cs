9610391d19c3aa2b99c860d9ab6b2746e5085ddb
doniyoraloxanov <doniyoraloxanov@gmail.com>
2025-02-22 18:38:35 +0500

    Refactoring

--- Business_Logic_Layer/BookBLL.cs
+++ Business_Logic_Layer/BookBLL.cs
@@ -1,34 +1,115 @@
-﻿using AutoMapper;
+﻿//using AutoMapper;
+//using Business_Logic_Layer.Models;
+//using Data_Access_Layer.Repository.Entities;
+
+//namespace Business_Logic_Layer
+//{
+//    public class BookBLL
+//    {
+
+//        private Data_Access_Layer.BookDAL _DAL;
+//        private Mapper _BooksMapper;
+//        private Mapper _AddBooksMapper;
+//        private Mapper _UpdateBooksMapper;
+
+
+//        public BookBLL()
+//        {
+//            _DAL = new Data_Access_Layer.BookDAL();
+//            var _configBook = new MapperConfiguration(cfg => cfg.CreateMap<Book, BooksModel>().ReverseMap());
+//            var _configAddBook = new MapperConfiguration(cfg => cfg.CreateMap<Book, AddBooksModel>().ReverseMap());
+//            var _configUpdateBook = new MapperConfiguration(cfg => cfg.CreateMap<Book, UpdateBooksModel>().ReverseMap());
+
+
+
+//            _BooksMapper = new Mapper(_configBook);
+//            _AddBooksMapper = new Mapper(_configAddBook);
+//            _UpdateBooksMapper = new Mapper(_configUpdateBook);
+
+
+//        }
+
+
+//        public async Task<List<string>> GetAllBooks(int pageNumber = 1, int pageSize = 1000)
+//        {
+//            List<string> bookTitles = await _DAL.GetAllBooks(pageNumber, pageSize);
+//            return bookTitles;
+//        }
+
+//        public async Task<BooksModel> GetBookById(Guid id)
+//        {
+//            var bookEntity = await _DAL.GetBookById(id);
+
+//            BooksModel bookModel = _BooksMapper.Map<Book, BooksModel>(bookEntity);
+
+
+//            return bookModel;
+
+//        }
+
+
+//        public async Task<UpdateBooksModel?> UpdateBook(Guid id, UpdateBooksModel updateBooks)
+//        {
+//            var bookModel = _UpdateBooksMapper.Map<UpdateBooksModel, Book>(updateBooks);
+
+//            var bookEntity = await _DAL.UpdateBook(id, bookModel);
+
+//            var updatedBookModel = _UpdateBooksMapper.Map<Book, UpdateBooksModel>(bookEntity);
+
+//            return updatedBookModel;
+//        }
+
+
+//        public async Task DeleteBook(Guid id) => await _DAL.DeleteBook(id);
+
+
+//        public async Task DeleteSoftBook(List<Guid> ids) => await _DAL.DeleteBooksBulk(ids);
+
+
+
+//        public async Task CreateBook(AddBooksModel booksModel)
+//        {
+//            var bookEntity = _AddBooksMapper.Map<AddBooksModel, Book>(booksModel);
+//            await _DAL.CreateBook(bookEntity);
+//        }
+
+
+//        public async Task CreateBooksBulk(List<AddBooksModel> addBooksModels)
+//        {
+//            var bookEntities = _AddBooksMapper.Map<List<AddBooksModel>, List<Book>>(addBooksModels);
+//            await _DAL.CreateBooksBulk(bookEntities);
+//        }
+
+//    }
+//}
+
+
+
+///////////////////////////////////
+
+using AutoMapper;
 using Business_Logic_Layer.Models;
 using Data_Access_Layer.Repository.Entities;
+
 
 namespace Business_Logic_Layer
 {
     public class BookBLL
     {
-
-        private Data_Access_Layer.BookDAL _DAL;
-        private Mapper _BooksMapper;
-        private Mapper _AddBooksMapper;
-        private Mapper _UpdateBooksMapper;
-
+        private readonly Data_Access_Layer.BookDAL _DAL;
+        private readonly IMapper _mapper;
 
         public BookBLL()
         {
             _DAL = new Data_Access_Layer.BookDAL();
-            var _configBook = new MapperConfiguration(cfg => cfg.CreateMap<Book, BooksModel>().ReverseMap());
-            var _configAddBook = new MapperConfiguration(cfg => cfg.CreateMap<Book, AddBooksModel>().ReverseMap());
-            var _configUpdateBook = new MapperConfiguration(cfg => cfg.CreateMap<Book, UpdateBooksModel>().ReverseMap());
-
-
-
-            _BooksMapper = new Mapper(_configBook);
-            _AddBooksMapper = new Mapper(_configAddBook);
-            _UpdateBooksMapper = new Mapper(_configUpdateBook);
-
-
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Book, BooksModel>().ReverseMap();
+                cfg.CreateMap<Book, AddBooksModel>().ReverseMap();
+                cfg.CreateMap<Book, UpdateBooksModel>().ReverseMap();
+            });
+            _mapper = config.CreateMapper();
         }
-
 
         public async Task<List<string>> GetAllBooks(int pageNumber = 1, int pageSize = 1000)
         {
@@ -39,46 +120,32 @@ namespace Business_Logic_Layer
         public async Task<BooksModel> GetBookById(Guid id)
         {
             var bookEntity = await _DAL.GetBookById(id);
-
-            BooksModel bookModel = _BooksMapper.Map<Book, BooksModel>(bookEntity);
-
-
-            return bookModel;
-
+            return _mapper.Map<BooksModel>(bookEntity);
         }
-
 
         public async Task<UpdateBooksModel?> UpdateBook(Guid id, UpdateBooksModel updateBooks)
         {
-            var bookModel = _UpdateBooksMapper.Map<UpdateBooksModel, Book>(updateBooks);
-
-            var bookEntity = await _DAL.UpdateBook(id, bookModel);
-
-            var updatedBookModel = _UpdateBooksMapper.Map<Book, UpdateBooksModel>(bookEntity);
-
-            return updatedBookModel;
+            var bookEntity = _mapper.Map<Book>(updateBooks);
+            var updatedEntity = await _DAL.UpdateBook(id, bookEntity);
+            return _mapper.Map<UpdateBooksModel>(updatedEntity);
         }
 
-
         public async Task DeleteBook(Guid id) => await _DAL.DeleteBook(id);
-     
 
         public async Task DeleteSoftBook(List<Guid> ids) => await _DAL.DeleteBooksBulk(ids);
-      
 
 
         public async Task CreateBook(AddBooksModel booksModel)
         {
-            var bookEntity = _AddBooksMapper.Map<AddBooksModel, Book>(booksModel);
+            var bookEntity = _mapper.Map<Book>(booksModel);
             await _DAL.CreateBook(bookEntity);
         }
 
 
         public async Task CreateBooksBulk(List<AddBooksModel> addBooksModels)
         {
-            var bookEntities = _AddBooksMapper.Map<List<AddBooksModel>, List<Book>>(addBooksModels);
+            var bookEntities = _mapper.Map<List<Book>>(addBooksModels);
             await _DAL.CreateBooksBulk(bookEntities);
         }
-
     }
 }
